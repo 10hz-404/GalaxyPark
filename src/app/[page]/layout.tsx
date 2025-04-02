9530789a0f40179ac22630b7d05909ac46ac7b13
10hz-404 <1208490466@qq.com>
2025-04-02 13:11:19 +0800

    添加Vercel：Analytics、SpeedInsights

--- src/app/[page]/layout.tsx
+++ src/app/[page]/layout.tsx
@@ -1,3 +1,12 @@
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+
 export default function AboutLayout(props: React.PropsWithChildren) {
-  return props.children
+  return (
+    <>
+      {props.children}
+      <Analytics />
+      <SpeedInsights />
+    </>
+  )
 }
